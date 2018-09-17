7a6e691f6bd40d8ccd827dd1555d2a5704c65166
antonio <antonio@pykiss.com>
2018-09-17 12:43:12 +0200

    preparando para publicar

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 /* global Package */
 
 Package.describe({
-  name: 'ocs:validarformulario',
+  name: 'hacknlove:validarformulario',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'valida formularios y muestra mensajes de error',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@hacknlove.github.com:hacknlove/validarFormulario.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -15,7 +15,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.7.0.5')
   api.use('ecmascript')
-  api.use('jquery', 'client')
+  api.use('jquery@1.0.0', 'client')
   api.use('less', 'client')
   api.addFiles('validarFormulario.less', 'client')
   api.mainModule('validarformulario.js', 'client')
